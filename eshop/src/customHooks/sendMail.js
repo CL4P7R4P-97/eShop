02b9affbc96b97ecf38ac6de45310d2e0a694c69
Kumar <ashish.ap.kumar@capgemini.com>
2023-11-13 03:41:18 +0530

    someCaq

--- eshop/src/customHooks/sendMail.js
+++ eshop/src/customHooks/sendMail.js
@@ -1,14 +1,13 @@
 
 export const SendMail = (body) => {
      
-             console.log(body);
-             console.log(JSON.stringify(body));
+             
                 fetch('https://tranquil-cocada-3b715c.netlify.app/sendOrderEmail', {
                   method: 'POST',
                   headers: {
                     'Content-Type': 'application/json',
                   },
-                  body: JSON.stringify(body),
+                  body:body,
                   
                 })
                   .then(response => response.json())
