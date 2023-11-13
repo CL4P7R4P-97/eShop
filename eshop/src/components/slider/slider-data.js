f9b14e308b8e3f62dbb1e2fce734b809f40b22c8
Kumar <ashish.ap.kumar@capgemini.com>
2023-11-13 15:29:44 +0530

    images

--- eshop/src/components/slider/slider-data.js
+++ eshop/src/components/slider/slider-data.js
@@ -1,21 +1,21 @@
 export const sliderData = [
     {
-      image: "https://i.ibb.co/CBGRLhG/bg-4.jpg",
+      image: "https://images.pexels.com/photos/2562992/pexels-photo-2562992.png",
       heading: "Shoes Hub",
       desc: "Up to 30% off on all onsale proucts.",
     },
     {
-      image: "https://i.ibb.co/cDLBk5h/bg-1.jpg",
+      image: "https://images.pexels.com/photos/932401/pexels-photo-932401.jpeg",
       heading: "Women Fashion",
       desc: "Up to 30% off on all onsale proucts.",
     },
     {
-      image: "https://i.ibb.co/HXjD3V0/bg-2.jpg",
+      image: "https://images.pexels.com/photos/3568518/pexels-photo-3568518.jpeg",
       heading: "Men Fashion",
       desc: "Up to 30% off on all onsale proucts.",
     },
     {
-      image: "https://i.ibb.co/H2FRmtV/bg-3.jpg",
+      image: "https://images.pexels.com/photos/59143/pexels-photo-59143.jpeg",
       heading: "Cool Gadgets",
       desc: "Up to 30% off on all onsale proucts.",
     },
